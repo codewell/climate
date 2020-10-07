0f32775ce9c9f9e7b33ed2a2927ac9c570ec29b0
Filip Johansson <filip.johansson@protonmail.com>
2020-10-07 16:42:44 +0200

    add COMMAND to smash init

--- init.sh
+++ init.sh
@@ -6,11 +6,14 @@ init () {
   read -rp 'DESCRIPTION=' description
   read -rp 'REPOSITORY=' repository
   read -rp 'MAIN=' main
+  read -rp 'COMMAND=' start_command
   
   { echo "NAME=${name}";
     echo "VERSION=${version}";
     echo "DESCRIPTION=${description}"; 
     echo "REPOSITORY=${repository}";
-    echo "MAIN=${main}"; } > "$(pwd)/.smash"
+    echo "MAIN=${main}"; 
+    echo "COMMAND=${start_command}"; 
+    } > "$(pwd)/.smash"
 }
 
